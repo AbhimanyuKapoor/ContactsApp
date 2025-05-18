adac0f23dd368379f96ab2df6791f66e86cdc143
AbhimanyuKapoor <abhimanyukapoor514@gmail.com>
2025-05-18 20:37:31 +0000

    Added relations between users & contacts, allowing users to view, edit and delete their own contacts. Changed UI of some elements.

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,9 +1,11 @@
 class ContactsController < ApplicationController
+  before_action :authenticate_user!
   before_action :set_contact, only: %i[ show edit update destroy ]
-
+  before_action :verify_user, only: %i[ show edit update destroy ]
+  
   # GET /contacts or /contacts.json
   def index
-    @contacts = Contact.all
+    @contacts = current_user.contacts
   end
 
   # GET /contacts/1 or /contacts/1.json
@@ -12,7 +14,11 @@ class ContactsController < ApplicationController
 
   # GET /contacts/new
   def new
-    @contact = Contact.new
+    # @contact = Contact.new
+    
+    # Automatically sets foreign key (user_id) for the contact
+    # Even if someone messes with the form field user_id, it doesn't change the user
+    @contact = current_user.contacts.build
   end
 
   # GET /contacts/1/edit
@@ -21,7 +27,8 @@ class ContactsController < ApplicationController
 
   # POST /contacts or /contacts.json
   def create
-    @contact = Contact.new(contact_params)
+    # @contact = Contact.new(contact_params)
+    @contact = current_user.contacts.build(contact_params)
 
     respond_to do |format|
       if @contact.save
@@ -65,6 +72,14 @@ class ContactsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def contact_params
-      params.require(:contact).permit(:first_name, :last_name, :email, :phone, :twitter)
+      params.require(:contact).permit(:first_name, :last_name, :email, :phone, :twitter, :user_id)
+    end
+
+    # Verify whether that particular contact is of the user 
+    # (@contact comes from set_contact method performed before this method)
+    def verify_user
+      if @contact.user != current_user
+        redirect_to contacts_path, notice: "You are not authorized for this action."
+      end
     end
 end
